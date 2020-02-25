fee120ca6289aadfb6b2f4ed02c9fedd2fe46b3b
Liu Dahu <runoope@outlook.com>
2020-02-25 23:18:11 +0800

    '-'

--- credit_card_default_rate/src.py
+++ credit_card_default_rate/src.py
@@ -1,12 +1,32 @@
+import pandas as pd
+
+# data read
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+data = pd.read_csv('./UCI_Credit_Card.csv')
+
+# data explore/fearture selection
+data.drop('ID', axis=1, inplace=True)
+target = data['default.payment.next.month']
+columns = data.columns.tolist()
+columns.remove('default.payment.next.month')
+features = data[columns].values
+train_x, test_x, train_y, test_y = train_test_split(features, target, test_size=0.33)
 
 rf = RandomForestClassifier()
-parameters = {'n_estimators': range(1, 11)}
+pipeline = Pipeline(
+    [('scaler', StandardScaler()),
+     ('randomforestclassifier', rf)]
+)
 
-clf = GridSearchCV(estimator=rf, param_grid=parameters)
-iris = load_iris()
-clf.fit(iris.data, iris.target)
+paragrams = {'randomforestclassifier__n_estimators': range(1, 11)}
+clf = GridSearchCV(estimator=pipeline, param_grid=paragrams)
+clf.fit(train_x, train_y)
 print(clf.best_score_)
 print(clf.best_params_)
+predict_y = clf.predict(test_x)
+print("准确率 %0.4lf" % accuracy_score(test_y, predict_y))
